080b3793a263c7a6a8b5d1feb01878943b2bf83b
Victor Tardif <victor.tardif1@gmail.com>
2022-01-20 14:54:25 +0100

    ADD setup mongodb & dotenv

--- index.mjs
+++ index.mjs
@@ -1,11 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/.env" });
 
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT;
 
 // Connection to MongoDB Atlas database
+mongoose.connect(process.env.DB_URI, (err) => {
+  if (err) console.log("[MongoDB connection ERROR]: " + err);
+  else console.log("Connected to MongoDB");
+});
 
 // Express Config
 app.use(cors());
