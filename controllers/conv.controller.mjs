2e0e26b96222184eaddb71bf2a76066c81e94273
Victor Tardif <victor.tardif1@gmail.com>
2022-01-27 15:50:27 +0100

    FIX populate msg&conv

--- controllers/conv.controller.mjs
+++ controllers/conv.controller.mjs
@@ -55,7 +55,7 @@ const getAllConvsFromUser = (req, res) => {
       if (err) res.status(400).send("Get user conversations ERROR: " + err);
       else res.status(200).send(docs);
     })
-    .populate("usersID", "firstname", "email", "motto");
+    .populate("usersID");
 };
 
 // GET one conversation (from current user)
@@ -72,7 +72,7 @@ const getOneConvFromUser = (req, res) => {
         else res.status(200).send(docs);
       }
     )
-    .populate("usersID", "firstname", "email", "motto");
+    .populate("usersID");
 };
 
 // ------ CRUD MESSAGE ------
@@ -107,7 +107,7 @@ const getOneMsg = (req, res) => {
         return res.status(200).send(message);
       }
     )
-    .populate("messages", "firstname", "email", "motto");
+    .populate("messages", "authorID");
 };
 
 // PUT add msg (to one conversation from current user)
