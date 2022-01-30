7ba5790b076ad3bc92b3b9b4d6bbee750676dfb8
Victor Tardif <victor.tardif1@gmail.com>
2022-01-31 00:15:54 +0100

    ADD pusher config & TEST mongoose.connection.once

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
+import Pusher from "pusher";
+// const Pusher = require("pusher");
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -10,23 +12,41 @@ import user_router from "./routes/user.router.mjs";
 import conv_router from "./routes/conv.router.mjs";
 import checkAuthToken from "./middlewares/auth.middlewares.mjs";
 
+// -- App config --
 const app = express();
 const PORT = process.env.PORT || 9000;
 
-// Connection to MongoDB Atlas
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set("trust proxy", 1);
+
+// -- Connection to MongoDB Atlas --
 mongoose.connect(process.env.DB_URI, (err) => {
   if (err) console.log("MongoDB connection Error: " + err);
   else console.log("Connected to MongoDB");
 });
 
-// PUSHER Config
+// -- PUSHER --
+// Config
+const pusher = new Pusher({
+  appId: "1340246",
+  key: "286aedadfa63e4354460",
+  secret: "52e2f6e509c61efa616c",
+  cluster: "eu",
+  useTLS: true,
+});
 
-// -- Configurations --
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.set("trust proxy", 1);
+pusher.trigger("my-channel", "my-event", {
+  message: "hello world",
+});
+
+// Connect pusher to mongoDB
+mongoose.connection.once("open", () => {
+  console.log("coucou c'est moi hihihi");
+});
 
 // -- Routes --
 // API Home-page
