27432ccf5d34b7c2cfe820b53023018fbb764fe6
Victor Tardif <victor.tardif1@gmail.com>
2022-01-31 01:28:49 +0100

    CONFIG mongodb changestream for pusher

--- index.mjs
+++ index.mjs
@@ -50,7 +50,7 @@ mongoose.connection.once("open", () => {
 
     // (Put) Add/edit/delete message
     if (change.operationType === "update") {
-      pusher.trigger("msgs", "updated", { convID: change.documentKey });
+      pusher.trigger("msgs", "updated", change.documentKey);
     }
 
     // Post new conversation
@@ -60,7 +60,7 @@ mongoose.connection.once("open", () => {
 
     // Delete conversation
     if (change.operationType === "delete") {
-      pusher.trigger("convs", "deleted", { convID: change.documentKey });
+      pusher.trigger("convs", "deleted", change.documentKey);
     }
   });
 });
